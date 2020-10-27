53561ab4382039aaaeddfc2c4cb3bed8e5c55275
Petar Perković <petar.perkovic15@gmail.com>
2020-10-27 13:02:40 +0100

    Populated Titles and Statuses tables in database

---
+++ Relaty/Data/Migrations/20201027111413_PopulateStatuses.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Relaty.Data.Migrations
+{
+    public partial class PopulateStatuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Statuses (Name) VALUES ('New')");
+            migrationBuilder.Sql("INSERT INTO Statuses (Name) VALUES ('In Progress')");
+            migrationBuilder.Sql("INSERT INTO Statuses (Name) VALUES ('Completed')");
+            migrationBuilder.Sql("INSERT INTO Statuses (Name) VALUES ('On Hold')");
+            migrationBuilder.Sql("INSERT INTO Statuses (Name) VALUES ('Canceled')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Relaty/Data/Migrations/20201027114127_PopulateTitles.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Relaty.Data.Migrations
+{
+    public partial class PopulateTitles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Analyst')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Architect')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Project Manager')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Front-end Developer')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Back-end Developer')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Full-stack Developer')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('UI/UX Developer')");
+            migrationBuilder.Sql("INSERT INTO Titles (Name) VALUES ('Database Developer')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
