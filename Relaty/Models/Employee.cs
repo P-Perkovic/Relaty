d4132de13d58f93ea39681d172f6da37010f6454
Petar Perković <petar.perkovic15@gmail.com>
2020-10-27 09:55:41 +0100

    Added employee and project controller and view, also added project, employee, status and title model.

---
+++ Relaty/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Relaty.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Mail { get; set; }
+
+        [ForeignKey("Title")]
+        public int TitleId { get; set; }
+
+        public Title Title { get; set; }
+
+        public List<Project> Projects { get; set; }
+    }
+}
---
+++ Relaty/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Relaty.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime? DateOfStart { get; set; }
+
+        [ForeignKey("Status")]
+        public int StatusId { get; set; }
+
+        public Status Status { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
